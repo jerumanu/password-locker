41da270204a2ceb7b46de95c985849a1b521177e
jerumanu <emmanueljeru200@gmail.com>
2020-11-21 15:18:30 +0300

    add class credetial

--- user.py
+++ user.py
@@ -8,3 +8,28 @@ class  User:
         
         self.username = username
         self.password = password
+
+ 
+    def save_user(self):
+
+        '''
+    user method saves contact objects into contact_list
+        '''
+
+        User.user__list.append(self)
+
+
+
+class Credetial:
+
+    credential_list = []
+
+    def __init__(self,account,username1,password1):
+
+
+
+        self.account = account
+        self.username1 = username1
+        self.password1 = password1
+
+
--- user__test.py
+++ user__test.py
@@ -1,5 +1,6 @@
 import unittest
-from user import User
+from user import User , Credetial
+
 
 class UserTest(unittest.TestCase):
 
@@ -11,9 +12,19 @@ class UserTest(unittest.TestCase):
 
         self.assertEqual(self.new__user.username,"jerumanu")
         self.assertEqual(self.new__user.password,91547712)
-    
-    def  test-save_user(self): 
+    def  test_save_user(self): 
+        self.new__user.save_user()
+        self.assertEqual(len(User.user__list),1)
 
+
+
+
+
+class CredetialTest(unittest.TestCase):
+    def setUp(self):
+
+        self.new_credetial = Credetial("twiter","manjeru",1234567)
+    def    
 
 if __name__ == '__main__':
     unittest.main()           
